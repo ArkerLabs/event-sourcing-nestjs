9375924453598b7914b58885808e80253458cb76
Juanjo Chust <nytyr@me.com>
2020-03-21 17:54:51 +0100

    ✨ Add provider

--- src/eventsourcing.providers.ts
+++ src/eventsourcing.providers.ts
@@ -2,12 +2,14 @@ import { ViewUpdater } from './view/view-updater';
 import { ViewEventBus } from './view/view-event-bus';
 import { StoreEventBus } from './store-event-bus';
 import { EventStore } from './eventstore';
+import { StoreEventPublisher } from './store-event-publisher';
 
 export function createEventSourcingProviders() {
     return [
         ViewUpdater,
         ViewEventBus,
         StoreEventBus,
+        StoreEventPublisher,
         EventStore,
     ];
 }
