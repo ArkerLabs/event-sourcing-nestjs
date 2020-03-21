bd32712b9e6f83e770ec30c29ef81f4d3e51db96
Juanjo Chust <nytyr@me.com>
2020-03-21 17:38:19 +0100

    ✨ Export event store class

--- src/eventsourcing.providers.ts
+++ src/eventsourcing.providers.ts
@@ -1,11 +1,13 @@
 import { ViewUpdater } from './view/view-updater';
 import { ViewEventBus } from './view/view-event-bus';
 import { StoreEventBus } from './store-event-bus';
+import { EventStore } from './eventstore';
 
 export function createEventSourcingProviders() {
     return [
         ViewUpdater,
         ViewEventBus,
         StoreEventBus,
+        EventStore,
     ];
 }
